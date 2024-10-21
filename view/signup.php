5c61f04d9da2179928c1aebe506bff1e5a5fc7cc
Princess Escobar <70127731+itzesco@users.noreply.github.com>
2024-10-21 14:10:23 +0800

    Update signup.php

--- view/signup.php
+++ view/signup.php
@@ -1,19 +1,38 @@
-<html>
-<head>
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="../public/css/input_form.css">
-</head>
-<body>
-    <div class="form">
-        <h1 class="title_card">SIGNUP</h1>
-        <form class="detail_form" action="home.html">
-            <input type="text" class="input" name="username" placeholder="Username"><br>
-            <input type="text" class="input" name="email" placeholder="Email"><br>
-            <input type="text" class="input" name="password" placeholder="Password"><br>
-            <input type="text" class="input" name="confirm_password" placeholder="Confirm Password"><br>
-            <br>
-            <button class="register_button" type="button">Register</button>
-        </form>
-    </div>
-</body>
-</html>
+<?php
+require 'config.php'; 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    if ($password !== $confirm_password) {
+        echo "Passwords do not match.";
+        exit;
+    }
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        echo "Username already taken.";
+    } else {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $username, $email, $hashed_password);
+        
+        if ($stmt->execute()) {
+            echo "Registration successful! You can now login.";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
